c027a40a4e218d56ab7ab0adbfed9fbc1857a47e
PppSsn <126223273+PppSsn@users.noreply.github.com>
2023-03-01 21:02:37 +0700

    If Else 2

---
+++ If Else 2.py
@@ -0,0 +1,11 @@
+n = int ( input (' ระบุคะแนนของคุณ' ) )
+if(n <50):
+  print('F Class')
+elif(n<60):
+  print('D Class')
+elif(n<70):
+  print('C Class')
+elif(n<80):
+  print('B Class')
+else:
+  print('A Class')
