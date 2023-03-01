96a936c2311491099d30fb31830b0c3650c23fba
PppSsn <126223273+PppSsn@users.noreply.github.com>
2023-03-01 21:21:23 +0700

    Create If Else 3.py

---
+++ If Else 3.py
@@ -0,0 +1,11 @@
+n = str(input('Username :'))
+
+b=str(input('Password :'))
+
+if n in ['admin']:
+   if b in ['Ad13n@23t']:
+      print('Welcome,You are Admin')
+   else:
+      print('You are not admin')
+else:
+   print('You are not admin')
